3cb9fb3755fc3d502dd4a7b597ff62d5254564cb
MaistrukHUB <vladyslavmaistruk07@gmail.com>
2023-10-28 11:32:59 +0300

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
+  const config = new DocumentBuilder()
+    .setTitle('Barbershop api')
+    .setDescription('This api for website barbershop')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('apiSwagger', app, document);
   await app.listen(port);
 }
 bootstrap();
